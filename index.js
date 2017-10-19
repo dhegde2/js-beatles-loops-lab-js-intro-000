e2689564b1ce1dc7fdeccd01a9023d513296fa64
Dhruv Hegde <desidhruv@gmail.com>
2017-10-19 21:06:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function theBeatlesPlay(array1, array2){
-let songs = []; 
+let songs = [];
 let a=0;
 if(array1.length > array2.length){
   a=array1.length;
 } else {
   a=array2.length;
 }
-for(int i=0;i<a;i++){ 
+for(let i=0;i<a;i++){
 array3=array1[i] + array2[i];
 }
   return songs; 
